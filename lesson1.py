e9cbca63efdca518f10af295ebac0fdb95e1523c
avtozarqua <138387084+avtozarqua@users.noreply.github.com>
2023-07-03 17:23:46 +0400

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,26 @@
+name ="avto"
+#name არის ცვლადი
+# =  არის ცვლადისთვის მნიშვნელობის მიმნიჭებული სიმბოლო
+# "avto" არის ცვლადისთვის მნიშვნელობა
+
+surname ="zarkua"
+
+print(name)
+# პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name = "avto" #ეს არის str (string) ტიპის ცვლადი
+age = 12 # ეს არის int ( integer ) მთელი რიცხვი
+
+height = 156.5 #ეს არის float ტიპის ცვლადი ( ათწილადი )
+#booLean (bool) ტიპის ცვლადი
+knows_programming = True  # True ან #False
+is_ugly = False  #snakecase(უბრალოდ წერის სტილი სტანდარტულად)
+
+isugly = False # ჯავასკრიპტული cameLcase
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+
+print(type(age))
+print(type(name))
+print(type(surname))
+print(type(height))
+print(type(knows_programming))
